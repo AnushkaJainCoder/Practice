8c4c3440d64ea719d9c42f6a8b1663b077d5c0aa
anushka.jain <anushka.jain@innovasolutions.com>
2025-02-20 14:18:30 +0530

    create spring.xml file

--- Java(SpringBoot, Spring)/DemoSpring/src/main/java/org/example/App.java
+++ Java(SpringBoot, Spring)/DemoSpring/src/main/java/org/example/App.java
@@ -1,4 +1,6 @@
 package org.example;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext()
+        Dev d = context.getBean(Dev.class);
+        d.build();
+//        System.out.println( "Hello World!" );
     }
 }
