f9beabf74344bd929246ffe3604b1b61b37aa65e
anushka.jain <anushka.jain@innovasolutions.com>
2025-02-24 14:59:52 +0530

    constructor and setter injection

--- Java(SpringBoot, Spring)/DemoSpring/src/main/java/org/example/App.java
+++ Java(SpringBoot, Spring)/DemoSpring/src/main/java/org/example/App.java
@@ -11,7 +11,12 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext( "spring.xml");
-//        Dev d =(Dev) context.getBean("dev");
+        Dev d =(Dev) context.getBean("dev");
+//        d.age = 20;
+//        d.setAge(12);
+        System.out.println(d.getAge());
+//        System.out.println();
+
 //        d.build();
 //        System.out.println( "Hello World!" );
     }
--- Java(SpringBoot, Spring)/DemoSpring/src/main/java/org/example/Dev.java
+++ Java(SpringBoot, Spring)/DemoSpring/src/main/java/org/example/Dev.java
@@ -3,8 +3,21 @@ package org.example;
 //@Component
 public class  Dev {
 
+    int age = 90;
+    public Dev(int age){
+        this.age = age;
+        System.out.println("parameter applied");
+    }
     public Dev(){
-        System.out.println("dev is running");
+        System.out.println("no parameter");
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void build(){
